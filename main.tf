e783c3412e104eeb62c3fccbc35e89ca0126f386
Samuel Molendolff Teixeira <contact@samuelmteixeira.dev>
2024-11-24 02:20:56 -0300

    update

--- main.tf
+++ main.tf
@@ -36,3 +36,22 @@ resource "aws_subnet" "private" {
     "kubernetes.io/role/internal-elb" = "1"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"   # Direcionando todo tráfego para a Internet
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.name}-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
